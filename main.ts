4a8a4032153083be5df4c6d07371fd4a21231865
Guillaume-Legault <93203429+Guillaume-Legault@users.noreply.github.com>
2021-11-22 09:19:47 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,18 +2,14 @@ let nombre = 0
 loops.everyInterval(500, function () {
     for (let index = 0; index <= 100; index++) {
         nombre = index
+        if (nombre % 3 == 0 && nombre % 5 == 0) {
+            basic.showString("FB")
+        } else if (nombre % 5 == 0) {
+            basic.showString("B")
+        } else if (nombre % 3 == 0) {
+            basic.showString("F")
+        } else {
+            basic.showNumber(nombre)
+        }
     }
-    if (nombre % 3 == 0) {
-        basic.showString("F")
-    } else if (nombre % 5 == 0) {
-        basic.showString("B")
-    } else if (nombre % 3 == 0 && nombre % 5 == 0) {
-        basic.showString("FB")
-    } else {
-    	
-    }
-    basic.showNumber(nombre)
-})
-basic.forever(function () {
-	
 })
